d84ba6a557bea9b6a6f2fa20c0ca0dffc3e095d3
sethnelson <sethnelson7525@gmail.com>
2026-03-31 11:33:40 -0400

    Initial commit

---
+++ imgdecomp.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+# The main calling function. Should return a single value (large int?) that represents the image
+def decompose(image_file):
+
+    return 0
+
+# Breaks images into tiles and returns an array of tiles
+def tiles(image):
+    return 0
+
+# takes an image of any size and returns that image downscaled to the specified nxn dimension
+def downscale(image, n):
+    return 0
+
+
