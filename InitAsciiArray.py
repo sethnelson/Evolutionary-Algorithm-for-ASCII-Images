0306fc1d2ba05d362133fe1aa8a2dbf4fd3dc8d5
Seth Nelson <sethnelson7525@gmail.com>
2026-04-06 16:18:58 -0400

    Finished ASCII image generation code. Produces a dictionary with build_dict function that maps ASCII codes to their average total luminance.

--- InitAsciiArray.py
+++ InitAsciiArray.py
@@ -9,25 +9,35 @@ from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
 # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
-def generate_images(width, height, ascii_code): # w and h given in pixels
+def char_to_img(width, height, ascii_code): # w and h given in pixels
     img = Image.new('L', (width, height), color=255) # L indicates grayscale. You know, like grayscaLe.
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("consola.ttf", 29) # on Windows check 'c:\%windir%\fonts' to see what's available (choose a monospace font)
-    text = 'A' #TODO: this should be an array of all ASCII chars we want to use (32-255) except [127, 129, 141, 143, 144, 157]
+    text = chr(ascii_code)
     draw.text((0, 0), text, fill=(0), font=font) # start position, content, fill color (white), font
-    img.show()
+    #img.show()
     return img
 
-#? Idea for how we can create images of all ASCII chars
-# exception_list = [127, 129, 141, 143, 144, 157] # these entries are blank on https://www.ascii-code.com/
-# for n in range(32, 256):
-#     if n in exception_list:
-#         continue
-#     generate_images(16, 24, n)
+def build_dict(tile_w, tile_h): #builds an ASCII dict of tiles (w x h)
+    exception_list = [127, 129, 141, 143, 144, 157] # these entries are blank on https://www.ascii-code.com/
+    char_dict = {}
+    for n in range(32, 256):
+        if n in exception_list:
+            continue
+        else:
+            img = char_to_img(tile_w, tile_h, n)
+            char_dict[f"{n}"] = f(img)
+    return char_dict
 
-img = generate_images(16, 24, 0) # manually adjusted to match font size of 29 and have dimensions that are factors of 4
-print(np.average(np.array(img)) / 255.0) # for now we're going to use a raw luminance value for comparing tiles. T
-#TODO need some way to calculate tile-size to font-size ratio
-#TODO add third argument that specifies start and end index of ASCII char set we want to use
+#! This function should be modified later to better represent image structure
+def f(tile): # the conversion function which produces a single real value to represent a tile
+    return np.average(np.array(tile)) / 255.0 # simple average luminance of tile
 
-#TODO After we generate the images, we need to decompose each and create the map of <ASCII index, image data
+exception_list = [127, 129, 141, 143, 144, 157]
+dict = build_dict(16, 24) # manually adjusted to match font size of 29 and have dimensions that are factors of 4
+for n in range(32, 256):
+        if n in exception_list:
+            continue
+        else:
+            v = dict[f"{n}"]
+            print(f"{n} : {v}")
